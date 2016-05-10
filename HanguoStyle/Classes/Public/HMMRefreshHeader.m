a25793566a2690fe8ac46386631fa744a6dc35cc
jay <jay.liu@kakaocrop.com>
2016-05-10 15:01:11 +0800

    修复bug1

---
+++ HanguoStyle/Classes/Public/HMMRefreshHeader.m
@@ -0,0 +1,41 @@
+//
+//  WGRefreshHeader.m
+//  Weego
+//
+//  Created by Kevin on 16/3/17.
+//  Copyright © 2016年 Weego. All rights reserved.
+//
+
+#import "HMMRefreshHeader.h"
+
+@implementation HMMRefreshHeader
+
+- (void)prepare
+{
+    [super prepare];
+    
+    // 设置普通状态的动画图片
+    NSMutableArray *idleImages = [NSMutableArray array];
+    for (NSUInteger i = 1; i<=44; i++) {
+        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"%zd", i]];
+        [idleImages addObject:image];
+    }
+    [self setImages:idleImages forState:MJRefreshStateIdle];
+    
+    // 设置即将刷新状态的动画图片（一松开就会刷新的状态）
+    NSMutableArray *refreshingImages = [NSMutableArray array];
+    for (NSUInteger i = 46; i<=64; i++) {
+        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"%zd", i]];
+        [refreshingImages addObject:image];
+    }
+    [self setImages:refreshingImages forState:MJRefreshStatePulling];
+    
+    // 设置正在刷新状态的动画图片
+         [self setImages:refreshingImages forState:MJRefreshStateRefreshing];
+    
+    // 隐藏时间
+    self.lastUpdatedTimeLabel.hidden = YES;
+    // 隐藏状态
+    self.stateLabel.hidden = YES;
+}
+@end
