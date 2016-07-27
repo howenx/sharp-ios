f326f6c7f674a523525aad40e3eddcf49059962d
wayne <wayne.wu@kakaocorp.com>
2016-07-27 09:58:47 +0800

    评价bug

---
+++ HanguoStyle/Classes/Goods/View/KKGgoodsViewCell.h
@@ -0,0 +1,13 @@
+//
+//  KKGgoodsViewCell.h
+//  HanguoStyle
+//
+//  Created by wayne on 16/7/27.
+//  Copyright © 2016年 liudongsheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKGgoodsViewCell : UITableViewCell
+
+@end
---
+++ HanguoStyle/Classes/Goods/View/KKGgoodsViewCell.m
@@ -0,0 +1,26 @@
+//
+//  KKGgoodsViewCell.m
+//  HanguoStyle
+//
+//  Created by wayne on 16/7/27.
+//  Copyright © 2016年 liudongsheng. All rights reserved.
+//
+
+#import "KKGgoodsViewCell.h"
+
+@implementation KKGgoodsViewCell
+
+-(instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        [self initSubViews];
+    }
+    return self;
+}
+
+-(void)initSubViews
+{
+}
+
+@end
